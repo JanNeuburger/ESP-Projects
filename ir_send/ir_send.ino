47dbf2e211a87cadb378d8eaa7bf2fca375b4339
enaidin <32167716+enaidin@users.noreply.github.com>
2018-06-25 01:10:43 +0200

    Add files via upload

---
+++ ir_send/ir_send.ino
@@ -0,0 +1,19 @@
+#include <IRremoteESP8266.h>
+#include <IRsend.h>
+
+IRsend irsend(0);
+
+void setup()
+{
+  irsend.begin();
+  Serial.begin(9600);
+}
+
+void loop() {
+  irsend.sendNEC(0xF4BA2988, 32);
+  Serial.println("sent 0xF4BA2988");
+  delay(2000);
+  irsend.sendNEC(0xE0E040BF, 32);
+  Serial.println("sent 0xE0E040BF");
+  delay(2000);
+}
