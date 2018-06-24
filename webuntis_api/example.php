1a2c4f089c963236a9c3c3f8174c39f0ed5d9105
Joules <jouleszapata@gmail.com>
2018-06-24 17:19:58 +0100

    Working webuntis API

---
+++ webuntis_api/example.php
@@ -0,0 +1,58 @@
+<?php
+    date_default_timezone_set('CET');
+    
+    $username = $_GET["username"];
+    $password = $_GET["password"];
+
+    require "webuntis.php";
+
+    $untis = new Webuntis();
+    
+    #auth('url', 'schoolname', 'username', 'password');
+    $untis->auth('mese.webuntis.com', 'htbla%20linz%20leonding', $username, $password);
+    
+    $table      = $untis->getTimetable();
+    $subjects   = $untis->getSubjects();
+    $teachers   = $untis->getTeachers();
+    $rooms      = $untis->getRooms();
+    
+    $currenttime = date('hi');
+    $currenttime = intval($currenttime);
+    
+    $endtime = 0;
+    $comparestarttime = 0;
+    
+    $foundcurrent = 0;
+    
+    print_r("Current UE: ");
+    for($i = 0; $i < sizeof($table['result']); $i++){
+        $endtime= intval($table['result'][$i]['endTime']);
+        $comparestarttime = intval($table['result'][$i]['startTime']);
+        if(($currenttime < $endtime) && ($currenttime > $comparestarttime)){
+            print_r(" now: ");
+            print_r($currenttime);
+            print_r(" end: ");
+            print_r($endtime);
+            print_r(" begin: ");
+            print_r($comparestarttime);
+            print_r(" room: ");
+            print_r($table['result'][$i]['ro'][0]['id']);
+            $foundcurrent = 1;
+        }
+    }
+    
+    /*print_r("Next UE: ");
+    if($foundcurrent == 1){
+        for($i = 0; $i < sizeof($table['result']); $i++){
+            if(intval($table['result'][$i]['endTime']) + 15){
+                
+            }
+        }
+    }*/
+    
+    
+    //print_r($table);
+    
+    $untis->logout();
+    
+?>
